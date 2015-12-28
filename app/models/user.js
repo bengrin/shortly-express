f82c909fdcba15728860c0a304322bb083331fba
Ben Grinberg <bengrin25@gmail.com>
2015-12-28 18:10:59 -0500

    user model

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,18 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+  initialize: function() {
+    this.on('creating', function(model, attrs, options) {
+      var password = model.get('password');
+      bcrypt.hash(password, 10, function(err, hash) {
+        //store username with hashed password
+        model.set('password', hash);
+      });
+    });
+  }
 });
 
 module.exports = User;
+
